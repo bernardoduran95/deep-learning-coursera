fc7a285481d7370388fe999e9b98bdd78f13d65d
Rodrigogzmn6 <rodrigoguzman6796@gmail.com>
2021-09-22 18:50:19 -0300

    prueba clock

--- Proyecto/shield_iot.vhd
+++ Proyecto/shield_iot.vhd
@@ -9,6 +9,8 @@ entity shield_iot is
 		--Auxiliares para 7 segmentos
 		seg1			: out std_logic;
 		seg2			: out std_logic;
+		clk_out		: out std_logic;
+		dato_aux		: out std_logic_vector (7 downto 0) := "10101010";
 		--Fin auxiliares para 7 segmentos
 		rst			: in std_logic;
 		rx_bt			: in std_logic;
@@ -47,6 +49,24 @@ architecture arch_shield_iot of shield_iot is
 		
 	end component;
 	
+	component divisor_frecuencia is
+	
+		generic (
+
+			escala : natural := 9	--Escala = f_in/f_out	
+		
+		);
+		
+		port (
+
+			clk				: 	in std_logic;	--Frecuencia de entrada en [Hz]
+			rst	  			: 	in std_logic;	--Reset
+			clk_out			: 	out std_logic
+
+		);
+	
+	end component;
+	
 begin
 
 	inst_bluetooth	:	bluetooth
@@ -56,6 +76,11 @@ begin
 	inst_wifi	:	wifi
 	
 		port map (clk, rst, rx_wf, dato_rx_wf, tx_wf);
+	
+	inst_clk_aux	: divisor_frecuencia
+
+		generic map (25000000)
+		port map (clk, rst, clk_out);
 	
 	seg1 <= '1';
 	seg2 <= '0';
